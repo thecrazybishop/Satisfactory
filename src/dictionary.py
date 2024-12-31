206253e0c40c307c2a30640593843b71e6d847fa
thecrazybishop <kelly.terry@wfs.aero>
2024-12-31 13:45:41 -0600

    added iron_ingot

--- src/dictionary.py
+++ src/dictionary.py
@@ -1,5 +1,12 @@
 from entities import Items
 
+iron_ingot = Items(
+    name="Iron Ingot",
+    prod_in="Smelter",
+    ipm=30,
+    sol_ingr1_name="Iron Ore",
+    sol_ingr1_ratio=1,
+)
 iron_plate = Items(
     name="Iron Plate",
     prod_in="Constructor",
@@ -41,6 +48,7 @@ mod_frame = Items(
     sol_ingr2_name="Iron Rod",
     sol_ingr2_ratio=6
 )
+
 
 items_dict = {
     "Iron Plate": iron_plate,
